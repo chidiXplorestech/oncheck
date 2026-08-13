7630aa27183b87badfae0d00cdbdbbade6f58afb
Chidiebube Uzodinma <mailchidiebubeuzodinma@gmail.com>
2026-08-13 11:17:16 +0100

    Refresh installed app for square mobile covers

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oncheck-shell-v2';
+const CACHE = 'oncheck-shell-v3';
 const CORE = ['./', './manifest.webmanifest', './oncheck-icon.svg'];
 
 self.addEventListener('install', event => {
