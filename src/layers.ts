9400f71383a59573f03609e5bb6c8e0c82e7f81b
Chidiebube Uzodinma <mailchidiebubeuzodinma@gmail.com>
2026-08-26 17:39:01 +0100

    Load Sync V2 realtime layers

--- src/layers.ts
+++ src/layers.ts
@@ -6,10 +6,13 @@ import './account-cloud-layer';
 import './calendar-tracker';
 import './media-mobile-runtime';
 import './media-layer';
+import './media-cloud-sync';
 import './workout-layer';
 import './workout-order';
 import './workout-visuals';
+import './extended-cloud-sync';
 import './pwa';
 
 document.documentElement.dataset.oncheckLayers = 'ready';
 document.documentElement.dataset.oncheckCalendarImport = 'ready';
+document.documentElement.dataset.ontrackSync = 'v2';
