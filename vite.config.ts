3d294bd03b3efda84d48612aa99d5d01654e835b
Chidiebube Uzodinma <mailchidiebubeuzodinma@gmail.com>
2026-09-13 17:48:57 +0100

    Deploy Rental Finder 26 public preview

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/oncheck/' : '/',
-  build: {
-    sourcemap: false,
-    target: 'es2022',
-  },
-});
